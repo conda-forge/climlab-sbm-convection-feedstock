b068e56cdf42d00e4302d0026f6813ac9a6d0824
Brian Rose <brose@albany.edu>
2026-05-02 15:13:05 -0400

    Let pip handle the build directly on windows

--- recipe/bld.bat
+++ recipe/bld.bat
@@ -1,14 +1,2 @@
-mkdir builddir
-
-meson setup %MESON_ARGS% builddir
-if errorlevel 1 (
-    type builddir\meson-logs\meson-log.txt
-    exit /b 1
-)
-
-%PYTHON% -m build --wheel --no-isolation --skip-dependency-check -Cbuilddir=builddir
-if errorlevel 1 exit /b 1
-
-for /f "delims=" %%i in ('dir /b dist\climlab_sbm_convection*.whl') do set WHL=%%i
-%PYTHON% -m pip install "dist\%WHL%"
+%PYTHON% -m pip install . --no-build-isolation --no-deps -vv
 if errorlevel 1 exit /b 1
