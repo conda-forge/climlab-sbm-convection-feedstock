6f298343d905f051d3968a06a99e7dd2e957f4fe
Brian Rose <brose@albany.edu>
2026-05-02 14:51:00 -0400

    New windows attempt

---
+++ recipe/bld.bat
@@ -0,0 +1,14 @@
+mkdir builddir
+
+%PYTHON% -m meson setup %MESON_ARGS% builddir
+if errorlevel 1 (
+    type builddir\meson-logs\meson-log.txt
+    exit /b 1
+)
+
+%PYTHON% -m build --wheel --no-isolation --skip-dependency-check -Cbuilddir=builddir
+if errorlevel 1 exit /b 1
+
+for /f "delims=" %%i in ('dir /b dist\climlab_sbm_convection*.whl') do set WHL=%%i
+%PYTHON% -m pip install "dist\%WHL%"
+if errorlevel 1 exit /b 1
