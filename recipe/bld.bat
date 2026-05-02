709445db450a708318d6e3015478706616f6d365
Brian Rose <brose@albany.edu>
2026-05-02 15:48:17 -0400

    More meson flag tweaks

--- recipe/bld.bat
+++ recipe/bld.bat
@@ -1,2 +1,2 @@
-%PYTHON% -m pip install . --no-build-isolation --no-deps -vv -Csetup-args=-Db_vscrt=from_buildenv
+%PYTHON% -m pip install . --no-build-isolation --no-deps -vv -Csetup-args=-Db_vscrt=none
 if errorlevel 1 exit /b 1
