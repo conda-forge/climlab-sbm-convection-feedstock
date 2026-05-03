33097fdd47e45b6fb2c35f3d7d77a9c364175575
Brian Rose <brose@albany.edu>
2026-05-03 17:47:18 -0400

    More attempts to fix link paths

--- recipe/bld.bat
+++ recipe/bld.bat
@@ -1,6 +1,11 @@
-REM flang 21 runtime (flang_rt.runtime.dynamic.lib) lives alongside clang_rt in
-REM lib/clang/<ver>/lib/windows; MSVC link.exe needs that directory in LIB.
-for /d %%i in ("%BUILD_PREFIX%\lib\clang\*") do set "LIB=%%i\lib\windows;%LIB%"
+REM Add conda-forge's flang-rt library directory to LIB so MSVC link.exe can
+REM find flang_rt.runtime.dynamic.lib if lld-link is still not used.
+set "LIB=%BUILD_PREFIX%\Library\lib;%LIB%"
+
+REM Ensure lld-link is in PATH (it lives in Library\bin alongside flang),
+REM then declare it as the Fortran linker so meson can actually find it.
+set "PATH=%BUILD_PREFIX%\Library\bin;%PATH%"
+set "FC_LD=lld-link"
 
 %PYTHON% -m pip install . --no-build-isolation --no-deps -vv -Csetup-args=-Db_vscrt=none
 if errorlevel 1 exit /b 1
