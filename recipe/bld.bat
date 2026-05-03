792293989ba7f8296ca79cbd14ce16745a163da0
Brian Rose <brose@albany.edu>
2026-05-03 12:32:51 -0400

    Library link path fixes

--- recipe/bld.bat
+++ recipe/bld.bat
@@ -1,5 +1,6 @@
-REM flang 21 requires lld-link (not MSVC link.exe) to resolve its runtime library
-set "FC_LD=lld-link"
+REM flang 21 runtime (flang_rt.runtime.dynamic.lib) lives alongside clang_rt in
+REM lib/clang/<ver>/lib/windows; MSVC link.exe needs that directory in LIB.
+for /d %%i in ("%BUILD_PREFIX%\lib\clang\*") do set "LIB=%%i\lib\windows;%LIB%"
 
 %PYTHON% -m pip install . --no-build-isolation --no-deps -vv -Csetup-args=-Db_vscrt=none
 if errorlevel 1 exit /b 1
