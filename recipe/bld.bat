0844a1152a6f7d469732f4dc6a3d2a16d16006bf
Brian Rose <brose@albany.edu>
2026-05-03 20:11:10 -0400

    better library paths

--- recipe/bld.bat
+++ recipe/bld.bat
@@ -1,14 +1,7 @@
-REM Diagnostic: show where flang finds its runtime library
-echo === flang resource dir ===
-flang.exe -print-resource-dir
-echo === flang search dirs ===
-flang.exe -print-search-dirs
-echo === verbose link of a minimal Fortran program ===
-(echo program test) > _test_f.f90
-(echo end program test) >> _test_f.f90
-flang.exe -v _test_f.f90 -o _test_f.exe 2>&1
-del _test_f.f90 _test_f.exe 2>nul
-echo === end diagnostics ===
+REM Replicate the libpaths flang adds when it drives a link, so MSVC link.exe
+REM (invoked directly by meson for the .pyd) finds flang_rt.runtime.dynamic.lib.
+for /d %%i in ("%BUILD_PREFIX%\Library\lib\clang\*") do set "LIB=%%i\lib\windows;%LIB%"
+set "LIB=%BUILD_PREFIX%\Library\lib;%LIB%"
 
 %PYTHON% -m pip install . --no-build-isolation --no-deps -vv -Csetup-args=-Db_vscrt=none
 if errorlevel 1 exit /b 1
